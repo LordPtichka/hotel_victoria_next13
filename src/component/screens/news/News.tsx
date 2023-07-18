9c125d62d9f211e63e11e6525353c29f8fa9be31
LordPtichka <daniilyakim@yandex.ru>
2023-07-18 22:34:13 +0300

    news

---
+++ src/component/screens/news/News.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/component/layout/Layout'
+import { FC } from 'react'
+
+const News: FC = () => {
+  return (
+    <Layout title="Home" description="Home">
+        <div>news</div>
+    </Layout>
+  )
+}
+
+export default News
---
+++ src/pages/News.tsx
@@ -0,0 +1,7 @@
+import News from '@/component/screens/news/News'
+import { NextPage } from 'next'
+
+const newsPage: NextPage = () => {
+    return (<News />) 
+}
+export default newsPage
