0df115fe83dfc588b15b117a82ba075265b83428
Ptichka <DaniilYakim@yandex.ru>
2023-07-20 00:32:25 +0300

    auth

--- src/component/screens/auth/AuthPage.tsx
+++ src/component/screens/auth/AuthPage.tsx
@@ -3,40 +3,52 @@ import style from "./AuthPage.module.scss"
 import Layout from '@/component/layout/Layout'
 import { AuthService } from '@/services/auth.service'
 import { IUser, IUserData } from '@/interface/user.interface'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
-
+import { useForm, SubmitHandler } from "react-hook-form"
 
 
 // =====================================================
 // =======> REMAKE <=================> REMAKE <=========
-const dataUser = async () => {
-  const data = await AuthService.getUser()
-  console.log(data)
-  return await data
-}
-dataUser()
+
+const [userData] = useState<IUser>({} as IUser)
+
+useEffect(() => {
+  const dataUser = async () => {
+    const data = await AuthService.getUser()
+    console.log(data)
+    return await data
+  }
+  dataUser()
+})
+
 // =====================================================
 // =====================================================
+
+
+
 
 const AuthPage: FC<IUserData> = ({user}) => {
+ 
+  const { register, handleSubmit } = useForm<IUser>()
 
-  const handleClick = () => {
-    console.log("click");
-  };
+  const onSubmit: SubmitHandler<IUser> = ( data ) => { // будет принимать поля из формы которые мы получим
+    console.log(data) // выполняем действие с данными которые пришли с формы
+    console.log(userData)
+    // тут должна быть отправка данных с формы на сервак для инициализации
+  }
 
 
   return (
     <Layout title="Home" description="Home">
-      <div>AuthPage</div>
-      <form  >
-        <input type="text" className={style.input}/>
-        <input type="text" className={style.input}/>
-        <button type='submit'>sign in</button>
-      </form>
+      <div>AuthPage</div>()
 
-      <button onClick={handleClick}> click </button>
 
+    <form onSubmit={handleSubmit(onSubmit)} >
+      <input type="text" {...register('login') }/>
+      <button>test</button>
+    </form>
+ 
 
   </Layout>
   )
--- src/component/screens/home/Home.tsx
+++ src/component/screens/home/Home.tsx
@@ -11,23 +11,7 @@ const Home: FC = () => {
 
       {/* ================================================================== */}
       {/* <header className="header-main"><video src="/static/video/video_promo.mp4" autoplay="" loop=""></video> */}
-        <div className="gradient">
-            <nav className="nav-line content">
-                <ul className="ul-nav">
-                    <li className="li-nav"><a href="/" className="nav-link font-for-link font-color-white">Главная</a></li>
-                    <li className="li-nav"><a href="/rooms" className="nav-link font-for-link font-color-white">Номера</a></li>
-                    <li className="li-nav"><a href="#contact" className="nav-link font-for-link font-color-white">Контакты</a></li>
-                    <li className="li-nav"><a href="#restaurant" className="nav-link font-for-link font-color-white">Рестораны</a></li>
-                    <li className="li-nav"><a href="#service" className="nav-link font-for-link font-color-white">Услуги</a></li>
-                    <a href="/booking" className="button btn-for-nav font-for-text font-color-white">Онлайн-бронирование</a>
-                </ul>
-            </nav>
-        </div>
-        
-        <section className="booking-block content">
-            {/* <div id="tl-search-form"><div style="line-height: 0; width: 100%;"><iframe id="tlFrame0.2360282354837624" name="tlFrame0.2360282354837624" frameborder="0" border="0" allowtransparency="yes" width="1" height="90" scrolling="no" style="max-width: none; min-width: 100%; width: 1px; height: 90px;"></iframe></div></div> */}
-        </section>
-    </header>
+
       {/* ================================================================== */}
       <Stocks />
 
