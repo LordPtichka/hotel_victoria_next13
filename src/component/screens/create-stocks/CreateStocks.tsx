a79a0a23153d3b813ab26dee8cc48efe4d8e76a0
LordPtichka <daniilyakim@yandex.ru>
2023-07-29 15:49:37 +0300

    _

--- src/component/screens/create-stocks/CreateStocks.tsx
+++ src/component/screens/create-stocks/CreateStocks.tsx
@@ -10,6 +10,7 @@ const CreateStocks: FC<IStockData> = ({ stocksAll }) => {
   const [stock, setStocks] = useState(stocksAll) // Состояние для хранения всех новостей
   const [title, setTitle] = useState("") // Состояние для хранения заголовка новости
   const [description, setDescription] = useState("") // Состояние для хранения описания новости
+  const [shortDescription, setShortDescription] = useState("") // Состояние для хранения описания новости
   const [image, setImage] = useState<File | null>(null) // Состояние для хранения выбранного изображения
 
   // Обработчик события при нажатии на кнопку "создать статью"
@@ -21,11 +22,15 @@ const CreateStocks: FC<IStockData> = ({ stocksAll }) => {
       const formData = new FormData()
       formData.append("title", title)
       formData.append("description", description)
-      formData.append("image", image as File)
+      formData.append("short_description", shortDescription)
+      formData.append("imagу", image as File)
+
+      console.log(title, description, shortDescription)
 
       const formDataObject = {}
       for (const [key, value] of formData.entries()) {
         formDataObject[key] = value
+        сщтыщдуюд
       }
       formData.append("img", `http://localhost:4200/${formDataObject.image.name}`)
 
@@ -33,7 +38,7 @@ const CreateStocks: FC<IStockData> = ({ stocksAll }) => {
       await axios.post("http://localhost:4200/Stocks/CreateStocks", formData)
 
       // Обновление состояния новостей после успешной отправки
-      setStocks([...stock, { id: stock.length + 1, title, description, img: "" }])
+      setStocks([...stock, { id: stock.length + 1, title, description, shortDescription, img: "" }])
 
       setStocks((prev) => {
         console.log(prev) // Вывод prev в консоль
@@ -48,6 +53,24 @@ const CreateStocks: FC<IStockData> = ({ stocksAll }) => {
       console.error(error)
     }
   }
+  
+  // ===========================================
+  // ===========================================
+
+ 
+  // const [previewImage, setPreviewImage] = useState(null);
+
+  // const handleImageChange = (event) => {
+  //   const file = event.target.files[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setPreviewImage(reader.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
+  
 
   return (
     <Layout title={"create"}>
@@ -57,7 +80,8 @@ const CreateStocks: FC<IStockData> = ({ stocksAll }) => {
         <div>
           <div className={style.offer}>
             <div className={style.img_for_offer} style={{ backgroundImage: `url()` }}>
-              <input type="file" placeholder="title" accept="image/*" onChange={(e) => setImage(e.target.files?.[0])} />
+              {/* ${previewImage} */}
+              <input type="file" placeholder="title" accept="image/*"  onChange={(e) => setImage(e.target.files?.[0])} />
             </div>
             <div className={style.title_offer}>
               <input className={`${style.input} ${style.title_offer}`} placeholder="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
@@ -65,12 +89,20 @@ const CreateStocks: FC<IStockData> = ({ stocksAll }) => {
             <div className={style.font_for_text}>
               <textarea className={style.input} placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} />
             </div>
+            <div className={style.font_for_text}>
+              <textarea className={style.input} placeholder="shortDescription" value={shortDescription} onChange={(e) => setShortDescription(e.target.value)} />
+            </div>
           </div>
         </div>
         <button onClick={createStocks}>создать статью</button>
       </form>
       <div>==============================</div>
-      {/* Отображение списка новостей */}
+
+      <div>
+        {/* <input type="file" onChange={handleImageChange} /> */}
+      </div>
+
+      {/* Отображение списка скидок */}
       {stock.length ? stock.map((stock) => <Stock key={stock.id} stock={stock} />) : <div>Stocks Not Found</div>}
     </Layout>
   )
