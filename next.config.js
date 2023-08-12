959d63f2383488f712e56fdb43a9f74eb5aca800
LordPtichka <DaniilYakim@yandex.ru>
2023-08-12 13:11:13 +0300

    _

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ["localhost"] },
+  env: {
+    HOST: process.env.HOST, // после прописи в файле .env нужно прописать тут
+  },
 }
 
 module.exports = nextConfig
